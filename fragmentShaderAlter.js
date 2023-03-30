654e57d1e7fae4b2678d7a8f6b6a60f7ea12528b
jonstead <72114726+jonstead@users.noreply.github.com>
2023-03-30 11:43:31 -0500

    Add files via upload

--- fragmentShaderAlter.js
+++ fragmentShaderAlter.js
@@ -1,5 +1,53 @@
 fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    }
+// Starts with Spiked Vertex Shader and adds in various parts
+// from other examples for a more interesting appearence
+// Plus messing around with 'random' colors
+precision highp float;
+
+varying vec3 vNormal;
+
+uniform vec3 color;
+
+uniform vec3 ambientColor;
+
+varying vec3 vPosition;
+
+uniform float time;
+
+//uv that we will receive from the vertex shader
+varying vec2 vUv;
+
+void main() {
+    float lightStrength = dot(normalize(vec3(sin(time), 1.5, 0) - vPosition), vNormal);
+    // gl_FragColor = vec4( (color ) * lightStrength + ambientColor, 1.0 ); // Default
+
+    // My random color experiment
+    // float a = 0.01;
+    // float b = 0.02;
+    // float c = 0.05;
+        
+    // for(float i = 0.0; i <= 10.0; i++)
+    // {
+    //     a += fract(sin(a)*1.0); // Function from https://thebookofshaders.com/10/
+    //     b += fract(sin(b)*1.0);
+    //     c += fract(sin(c)*1.0);
+    //     if(a >= 1.0)
+    //     {
+    //         a = .01;
+    //     }
+    //     if(b >= 1.0)
+    //     {
+    //         b = .01;
+    //     }
+    //     if(c >= 1.0)
+    //     {
+    //         c = .01;
+    //     }
+    //     gl_FragColor = vec4( a, b, c, 1.0 );
+    // }
+
+    gl_FragColor = vec4( vec3(vUv, 0), 1.0 ); //From Shader with UVs
+
+    // gl_FragColor = vec4( (sin(time)*color + vec3(sin(vUv.x * 1000.0), sin(vUv.y*500.0),0.0 ))* lightStrength + ambientColor, 1.0 ); //Checkerboard color
+}
   `;
--- vertexShaderAlter.js
+++ vertexShaderAlter.js
@@ -1,8 +1,37 @@
 vsSource = `
-    attribute vec4 aVertexPosition;
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    void main() {
-      gl_Position = mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1) * uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    }
+// Starts with Spiked Vertex Shader and adds in various parts
+// from other examples for a more interesting appearence
+uniform mat4 modelViewMatrix;   
+uniform mat4 projectionMatrix;  
+attribute vec3 position;        
+uniform mat4 modelMatrix;       
+
+attribute vec3 normal;
+
+varying vec3 vNormal;
+attribute vec2 uv;
+
+varying vec3 vPosition;
+
+uniform float time;
+varying vec2 vUv;
+
+void main() {
+    vNormal = normalize( vec3( modelMatrix * vec4(normal, 0.0 ) )).xyz;
+    
+    vUv = uv;
+
+    vec3 vPosition = ( modelMatrix * vec4( position, 1.0 )).xyz;
+    //Default 
+    // gl_Position = projectionMatrix * modelViewMatrix * vec4( position + vNormal * cos(uv.x*1000.0)*cos(time), 1.0 );
+
+    // Added moving vertices to go back and forth
+    // gl_Position = projectionMatrix * modelViewMatrix * vec4( position + vec3(-sin(time), 0.0, 0.0) + vNormal * cos(uv.x*1000.0)*cos(time), 1.0 );
+    // Or can use Distorted UV shader version
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position + vec3(sin(time)*uv.x, 0.0, 0.0) + vNormal * cos(uv.x*1000.0)*cos(time), 1.0 );
+    //Just Moveing
+    // gl_Position = projectionMatrix * modelViewMatrix * vec4( position + vec3(-sin(time), 0.0, 0.0), 1.0 );
+    // And just distortion
+    // gl_Position = projectionMatrix * modelViewMatrix * vec4( position + vec3(-sin(time)*uv.x, 0.0, 0.0), 1.0 );
+}
 `;
